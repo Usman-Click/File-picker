15fdb64abfb456ea960f33be1aafa51c98a6e73d
usmaan.code <uadamuharuna@gmail.com>
2022-09-08 03:27:34 +0100

    Initial commit

---
+++ app/src/main/java/com/nomedia/filepicker/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nomedia.filepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.developer.filepicker.file.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
